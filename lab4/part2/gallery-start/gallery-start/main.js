7ddf4a969038ca5c067804ac9699b4ffb287ebba
ElisabethBurgess <elisabeth.burgess@dcmail.ca>
2025-03-17 22:45:02 -0400

    lab4 part2 - 5th commit
    changed the filenames array to the actual file paths
    Added the click event listener to the photos in the thumb bar
    Created the event listener and function to change the mode of the photo

--- lab4/part2/gallery-start/gallery-start/main.js
+++ lab4/part2/gallery-start/gallery-start/main.js
@@ -12,19 +12,39 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const filenames = [pic1.jpg, pic2.jpg, pic3.jpg, pic4.jpg, pic5.jpg];
+const filenames = ['images/pic1.jpg', 'images/pic2.jpg', 'images/pic3.jpg', 'images/pic4.jpg', 'images/pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
 const alternativeText = ["Closeup of a human eye", "Closeup of sand", "Closeup of purple and white flowers", "Egyption drawings and hyroglyphics", "A brown butterfly"];
 
 /* Looping through images */
 for (let i = 0; i < filenames.length; i++){
+
     const newImage = document.createElement('img');
     newImage.setAttribute('src', filenames[i]);
     newImage.setAttribute('alt', alternativeText[i]);
-    thumbBar.appendChild(newImage);   
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener("click", (newImage) => {
+        displayedImage.setAttribute('src', filenames[i]);
+        displayedImage.setAttribute('alt', alternativeText[i]);
+    });   
    
 }
 
-
 /* Wiring up the Darken/Lighten button */
+
+function changeMode(){
+    const btnClass = btn.getAttribute("class");
+    if (btnClass === "dark"){
+        btn.setAttribute("class", "light");
+        btn.textContent = "Lighten";
+        overlay.style.backgroundColor = "rgb(0 0 0 / 50%)";
+    } else {
+        btn.setAttribute("class", "dark");
+        btn.textContent = "Darken";
+        overlay.style.backgroundColor = "rgb(0 0 0 / 0%)";
+    }
+
+}
+
+btn.addEventListener("click", changeMode);
